90ef9176651135f8b41270669aa33b5c3674d4af
bhavinsen <senbhavin@gmail.com>
2023-02-23 13:07:46 +0530

    Changes added

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8080"
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
